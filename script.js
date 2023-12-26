438991ec083a99d9d6bb98c2c52f05898e3999df
Kabuto228 <mr.geimer120@gmail.com>
2023-12-26 12:16:15 +0500

    js_add

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let heroes = [["Axe", "Tank"], ["Crystal Maiden", "Support"]]; 
+
+function displayHeroes()  {
+  let heroesContainer = document.getElementById("heroesContainer");
+
+  heroesContainer.innerHTML = ""; 
+
+  for (let i = 0; i < heroes.length; i++) {
+    let heroDiv = document.createElement("div");
+
+    heroDiv.innerHTML = `<h3>${heroes[i][0]}</h3><p>${heroes[i][1]}</p>`;
+
+    heroesContainer.appendChild(heroDiv);
+  }
+}
+
+function addHero() {
+
+    let nameInput = document.getElementById("heroName");
+    let classInput = document.getElementById("heroClass");
+
+    let newHero = [nameInput.value, classInput.value];
+
+    heroes.push(newHero);
+
+    displayHeroes();
+
+    nameInput.value = "";
+    classInput.value = "";
+} 
+
+displayHeroes();
+
+document.getElementById("addButton").addEventListener("click", addHero); 
